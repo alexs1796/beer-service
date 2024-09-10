6a3c7ec9f83d63c7c4ae9c89e74033ff8ab09666
alexs1796 <63447420+alexs1796@users.noreply.github.com>
2024-09-10 07:34:01 +0300

    Basic tests for the controller

--- src/test/java/com/example/beerservice/BeerServiceApplicationTests.java
+++ src/test/java/com/example/beerservice/BeerServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.beerservice;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class BeerServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
